773c5511fe92d794a24f0260ea02b610fe46e12b
Vladimir Ivashkin <illusionww@gmail.com>
2021-03-28 18:08:18 +0300

    update tests

--- tests/tests_gpuparallel.py
+++ tests/tests_gpuparallel.py
@@ -6,6 +6,8 @@ from gpuparallel import GPUParallel
 
 class TestGPUParallel(unittest.TestCase):
     def test_init(self):
+        print('Run Test: test_init')
+
         def init(worker_id=None, gpu_id=None):
             global result
             result = worker_id
@@ -14,22 +16,28 @@ class TestGPUParallel(unittest.TestCase):
             global result
             return result
 
-        results = GPUParallel(n_gpu=2, init_fn=init)(perform for _ in range(100))
+        results = GPUParallel(n_gpu=2, init_fn=init)(perform for _ in range(10))
         self.assertEqual(set(results), {0, 1})
 
     def test_results(self):
+        print('Run Test: test_results')
+
         def perform(idx, worker_id=None, gpu_id=None):
+            print(f'Perform {idx}')
             return idx
 
-        true_seq = list(range(100))
+        true_seq = list(range(10))
         results = GPUParallel(n_gpu=2)(partial(perform, idx) for idx in true_seq)
         self.assertEqual(sorted(results), true_seq)
 
     def test_multicall(self):
+        print('Run Test: test_multicall')
+
         def perform(idx, worker_id=None, gpu_id=None):
+            print(f'Perform {idx}')
             return idx
 
-        true_seq1, true_seq2 = list(range(100)), list(range(100, 200))
+        true_seq1, true_seq2 = list(range(10)), list(range(10, 20))
         gp = GPUParallel(n_gpu=2)
         results = gp(partial(perform, idx) for idx in true_seq1)
         self.assertEqual(sorted(results), true_seq1)
