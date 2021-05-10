580e746330fc12bdb1e6720c87503f6473c0beac
vlivashkin <Ga12m10p7>
2021-05-10 11:59:13 +0300

    debug synced mode for n_gpu=0

--- gpuparallel/gpuparallel.py
+++ gpuparallel/gpuparallel.py
@@ -36,6 +36,7 @@ class GPUParallel:
         :param n_gpu:
             Number of GPUs to use. The library doesn't check if GPUs really available, it is simply provide
             consistent ``worker_id`` and ``gpu_id`` to both ``init_fn`` and task functions.
+            ``n_gpu = 0`` turns on synced debug mode.
         :param n_workers_per_gpu: Number of workers on every GPU.
         :param init_fn:
             Function which will be called during worker init.
@@ -48,6 +49,7 @@ class GPUParallel:
         :param progressbar: Allow to use tqdm progressbar.
         :param ignore_errors: Either ignore errors inside tasks or raise them.
         """
+        self.debug_mode = n_gpu == 0
         self.n_gpu = n_gpu
         self.n_workers_per_gpu = n_workers_per_gpu
         self.init_fn = init_fn
@@ -55,26 +57,39 @@ class GPUParallel:
         self.progressbar = progressbar
         self.ignore_errors = ignore_errors
 
-        m = Manager()
-        self.gpu_queue = m.Queue()
-        for gpu_id in range(self.n_gpu):
-            for idx in range(self.n_workers_per_gpu):
-                worker_id = gpu_id * self.n_workers_per_gpu + idx
-                self.gpu_queue.put((worker_id, gpu_id))
+        if not self.debug_mode:
+            m = Manager()
+            self.gpu_queue = m.Queue()
+            for gpu_id in range(self.n_gpu):
+                for idx in range(self.n_workers_per_gpu):
+                    worker_id = gpu_id * self.n_workers_per_gpu + idx
+                    self.gpu_queue.put((worker_id, gpu_id))
 
-        initializer = partial(_init_worker, gpu_queue=self.gpu_queue, init_fn=init_fn, verbose=self.verbose)
-        self.pool = Pool(processes=self.n_gpu * self.n_workers_per_gpu, initializer=initializer,
-                         maxtasksperchild=None)
+            initializer = partial(_init_worker, gpu_queue=self.gpu_queue, init_fn=init_fn, verbose=self.verbose)
+            self.pool = Pool(processes=self.n_gpu * self.n_workers_per_gpu, initializer=initializer,
+                             maxtasksperchild=None)
 
-        self.result_queue = m.Queue()
+            self.result_queue = m.Queue()
+        else:  # debug mode; run init in the same process
+            init_fn(worker_id=0, gpu_id=0)
 
     def __del__(self):
         """
         Created pool will be freed only during this destructor.
         This allows to use ``__call__`` multiple times with the same initialized workers.
         """
-        self.pool.close()
-        self.pool.join()
+        if not self.debug_mode:
+            self.pool.close()
+            self.pool.join()
+
+    def _call_sync(self, tasks: Iterable) -> List:
+        tasks = list(tasks)
+        if self.progressbar:
+            from tqdm.auto import tqdm
+            results = [task(worker_id=0, gpu_id=0) for task in tqdm(tasks)]
+        else:
+            results = [task(worker_id=0, gpu_id=0) for task in tasks]
+        return results
 
     def __call__(self, tasks: Iterable) -> List:
         """
@@ -85,6 +100,10 @@ class GPUParallel:
             Functions must have parameters ``worker_id`` and ``gpu_id`` (or ``**kwargs``).
         :return: List of results
         """
+
+        if self.debug_mode:
+            return self._call_sync(tasks)
+
         n_tasks = 0
         for task in tasks:
             self.pool.apply_async(_run_task, (task, self.result_queue, self.ignore_errors))
